f58118f683c8660e451bc2ca741c1a2e920f9f06
Nikita Shabalin <nike.shabalin@gmail.com>
2020-12-07 16:48:38 +0300

    adding truncate and fix bug

---
+++ magn/src/main/java/inner/conToDB/Check.java
@@ -0,0 +1,14 @@
+package inner.conToDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+
+
+public class Check {
+
+	
+}
